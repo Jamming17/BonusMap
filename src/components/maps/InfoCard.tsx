8043285325b29fec1ca493f2457a729cea64f1bf
Jamming17 <jackchiplin@outlook.com>
2026-03-08 14:51:29 +0000

    Added info and nearby information to a few stores and displaying for them

--- src/components/maps/InfoCard.tsx
+++ src/components/maps/InfoCard.tsx
@@ -14,7 +14,7 @@ function InfoCard({ shop, onClose }: InfoCardProps) {
                 {shop !== null &&
                     <>
                         <button className="card-close-button" onClick={onClose}>✕</button>
-                        <div>
+                        <div className="card-information">
                             <div className="shop-title-container">
                                 <img
                                     src={bonusPig}
@@ -25,14 +25,37 @@ function InfoCard({ shop, onClose }: InfoCardProps) {
                                 <div className="shop-title">{shop.name}</div>
                             </div>
 
-                            <div className="shop-address-container">
-                                <div className="shop-address">
+                            <div className="info">
+                                <div className="info-bold">
                                     {`${shop.postcode} ${shop.city}, ${shop.country}`}
                                 </div>
                                 <div className="shop-address-coordinates">
                                     {`Latitude: ${shop.latitude}, Longitude: ${shop.longitude}`}
                                 </div>
                             </div>
+                            <div className="bonus-info-outer-container">
+                                <img className="bonus-thumbnail" src={`/shops/${shop.imageName}.jpg`} alt={`Image of ${shop.name}`} />
+                                <div className="bonus-info-container">
+                                    <div className="info">Opening times: <span className="info-bold">{shop.openingTimes}</span></div>
+                                    {shop.dateOpened && <div className="info">Opened: <span className="info-bold">{shop.dateOpened}</span></div>}
+                                    {shop.dateClosed && <div className="info info-red">Closed: <span className="info-bold">{shop.dateClosed}</span></div>}
+                                    {shop.info && <div className="info">Fun fact: {shop.info}</div>}
+                                </div>
+                            </div>
+                            <div className="nearby-title">Nearby:</div>
+                            {shop.nearby &&
+                                <div className="nearby-outer-container">
+                                    {shop.nearby.map((n, index) => (
+                                        <div key={index} className="nearby-container info">
+                                            <img className="nearby-thumbnail" src={`/nearby/${n.imageName}.jpg`} alt={`Image of ${n.name}`} />
+                                            <div className="nearby-details">
+                                                <div className="info-bold">{n.name}</div>
+                                                <div className="nearby-description">{n.description}</div>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            }
                         </div>
                     </>
                 }
--- src/utils/bonusJsonParser.ts
+++ src/utils/bonusJsonParser.ts
@@ -1,11 +1,23 @@
 export type BonusShop = {
-    id:        string;
-    name:      string;
-    country:   "Iceland" | "Faroe Islands";
-    city:      string;
-    postcode:  string;
-    latitude:  number;
-    longitude: number;
+    id:           string;
+    name:         string;
+    country:      "Iceland" | "Faroe Islands";
+    city:         string;
+    postcode:     string;
+    latitude:     number;
+    longitude:    number;
+    imageName:    string;
+    info:         string;
+    openingTimes: string;
+    dateOpened:   string;
+    dateClosed:   string;
+    nearby:       Array<BonusNearby>;
+}
+
+export type BonusNearby = {
+    name:         string;
+    description:  string;
+    imageName:    string;
 }
 
 export function parseJSON(jsonInput: string): Array<BonusShop> {
@@ -22,13 +34,29 @@ export function parseJSON(jsonInput: string): Array<BonusShop> {
 function BonusTypeGuard(input: any): input is BonusShop {
     return (
         input &&
-        typeof(input)          === "object" &&
-        typeof(input.id)       === "string" &&
-        typeof(input.name)     === "string" &&
+        typeof(input)              === "object" &&
+        typeof(input.id)           === "string" &&
+        typeof(input.name)         === "string" &&
         (input.country === "Iceland" || input.country === "Faroe Islands") &&
-        typeof(input.city)     === "string" &&
-        typeof(input.postcode) === "string" &&
-        typeof(input.latitude) === "number" &&
-        typeof(input.longitude) === "number"
+        typeof(input.city)         === "string" &&
+        typeof(input.postcode)     === "string" &&
+        typeof(input.latitude)     === "number" &&
+        typeof(input.longitude)    === "number" &&
+        typeof(input.imageName)    === "string" &&
+        typeof(input.info)         === "string" &&
+        typeof(input.openingTimes) === "string" &&
+        typeof(input.dateOpened)   === "string" &&
+        typeof(input.dateClosed)   === "string" &&
+        input.nearby.every((nearbyI: any) => BonusNearbyTypeGuard(nearbyI))
     );
+}
+
+function BonusNearbyTypeGuard(input: any): input is BonusNearby {
+    return (
+        input &&
+        typeof(input)             === "object" &&
+        typeof(input.name)        === "string" &&
+        typeof(input.description) === "string" &&
+        typeof(input.imageName)   === "string"
+    )
 }
